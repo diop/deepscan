2b5a4ffa9bf650450a64a75031cfeeb3ee9fde2a
diop <fodesdiop@gmail.com>
2020-05-31 12:51:52 +0000

    Switch to Streamlit

--- app.py
+++ app.py
@@ -1,21 +1,48 @@
-import os
-import sys
-from flask import Flask
-from flask import render_template
+import os, sys
+import numpy as np
+import streamlit as st
+from PIL import Image
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+cover = Image.open("assets/cover.png")
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-@app.route('/')
+@st.cache
+def load_image(img):
+    im = Image.open(img)
+    return im
+
+
+def detect_covid19(uploaded_xray):
+    return uploaded_xray
+
+
 def main():
-    return 'Superscan - Medical Imaging x Deep Learning'
+    st.image(cover, use_column_width=True)
+    st.title("Covid-19 Detection via Chest X-Ray")
+    st.text("by Fodé Diop")
 
-@app.route('/upload-image', methods=['GET', 'POST'])
-def upload_image():
-    return render_template('public/upload_image.html')
+    options = ["Detection", "About"]
+    choice = st.sidebar.selectbox("Select Option", options)
+
+    if choice == "Detection":
+        image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+
+        if image_file is not None:
+            uploaded = Image.open(image_file)
+            st.text("Uploaded Chest X-Ray")
+
+        if st.button("Detect Covid-19"):
+            # result_img = detect_covid19(uploaded)
+            st.image(uploaded)
+
+            st.success(f"Successfuly Processed X-Ray Image")
+
+    elif choice == "About":
+        st.subheader("About Superscan")
+        st.markdown("Built with Streamlit by [Fodé Diop](https://www.github.com/diop)")
+        st.text("© Copyright 2020 Fodé Diop - MIT License")
+        st.success("Dakar Institute of technology")
 
 
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    main()
