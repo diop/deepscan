3d3183518c7b0da024425c4db2410cf711de0946
diop <fodesdiop@gmail.com>
2020-06-03 01:12:22 +0000

    WIP

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import os, sys
 import numpy as np
 import streamlit as st
 from PIL import Image
+import tensorflow
+import tensorflow.keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 
 cover = Image.open("assets/cover.png")
+model = load_model("models/superscan-model.h5")
 
 
 @st.cache
@@ -13,7 +18,11 @@ def load_image(img):
 
 
 def detect_covid19(uploaded_xray):
-    return uploaded_xray
+    img = image.load_img(uploaded_xray, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    prediction = model.predict_classes(img)
+    return prediction
 
 
 def main():
@@ -25,17 +34,17 @@ def main():
     choice = st.sidebar.selectbox("Select Option", options)
 
     if choice == "Detection":
-        image_file = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+        image_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
         if image_file is not None:
             uploaded = Image.open(image_file)
-            st.text("Uploaded Chest X-Ray")
+            st.image(uploaded, caption="Uploaded Image.", use_column_width=True)
 
         if st.button("Detect Covid-19"):
-            # result_img = detect_covid19(uploaded)
-            st.image(uploaded)
-
-            st.success(f"Successfuly Processed X-Ray Image")
+            st.write("")
+            st.write("Detecting...")
+            pred = detect_covid19(uploaded)
+            st.write(pred)
 
     elif choice == "About":
         st.subheader("About Superscan")
