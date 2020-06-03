ea5fe8c13a4f099b739ed5acee1a7f20cb3ec782
diop <fodesdiop@gmail.com>
2020-06-03 03:00:47 +0000

    Fix image upload

--- app.py
+++ app.py
@@ -9,19 +9,22 @@ from tensorflow.keras.preprocessing import image
 
 cover = Image.open("assets/cover.png")
 model = load_model("models/superscan-model.h5")
+url = "https://covid-data-bucket.s3.amazonaws.com/covid-19/01E392EE-69F9-4E33-BFCE-E5C968654078.jpeg"
 
 
-@st.cache
-def load_image(img):
-    im = Image.open(img)
-    return im
+# @st.cache
+# def load_image(img):
+#     im = Image.open(img)
+#     im.tobytes("xbm", "rgb")
+#     return im
 
 
 def detect_covid19(uploaded_xray):
-    img = image.load_img(uploaded_xray, target_size=(224, 224))
-    img = image.img_to_array(img)
+    st.write(uploaded_xray)
+    img = image.img_to_array(uploaded_xray)
     img = np.expand_dims(img, axis=0)
-    prediction = model.predict_classes(img)
+    data = np.asarray(img, dtype="int32")
+    prediction = model.predict_classes(data)
     return prediction
 
 
@@ -37,7 +40,7 @@ def main():
         image_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
         if image_file is not None:
-            uploaded = Image.open(image_file)
+            uploaded = image.load_img(image_file, target_size=(224, 224))
             st.image(uploaded, caption="Uploaded Image.", use_column_width=True)
 
         if st.button("Detect Covid-19"):
